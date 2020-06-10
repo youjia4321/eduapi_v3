e587f898639e777fc8009bfd18dec1a2d99b8354
youjia <youjia4321@163.com>
2020-06-10 10:23:38 +0800

    添加redis缓存功能

--- utils/cache.py
+++ utils/cache.py
@@ -8,7 +8,8 @@
 from utils import rd
 
 
-def save_token(token, user_id):  # 保存token及对应的user_id
+def save_token(token, user_id):
+    # 保存token及对应的user_id
     # 在redis数据库存储token -> user_id 信息，设置过期时间ex
     rd.set(token, user_id, ex=3*24*3600)
     # rd.expire(token, 3*24*3600)
